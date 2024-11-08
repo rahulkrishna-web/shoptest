800162cdd960de15cc817f263467135eba016333
Rahul Krishna <rahulkrishna.web@gmail.com>
2024-11-08 23:26:13 +0530

    Add title

--- src/components/dash.js
+++ src/components/dash.js
@@ -6,11 +6,15 @@ import {NavMenu} from '@shopify/app-bridge-react';
 const Dash = () => {
   return (
     <div>
+    <TitleBar title="Events">
+      <button variant="primary">Create event</button>
+    </TitleBar>
       <NavMenu>
       <a href="/" rel="home">
         Home
       </a>
       <a href="/events">Events</a>
+      <a href="/tickets">Tickets</a>
       <a href="/settings">Settings</a>
         </NavMenu>
     </div>
